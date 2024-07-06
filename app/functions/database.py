bd198c8dad83bcb2c8765c35fa9395e4a9120c50
Frankie Willcox Esparza <frankie@goob.lan>
2024-07-06 14:55:25 -0700

    add thyme architecture & database diagrams

--- app/functions/database.py
+++ app/functions/database.py
@@ -3,7 +3,7 @@ from app.customizations.add_priority_and_highlight import REPOS_WITH_PRIORITY_OR
 from app.customizations.settings import (
     FILE_TYPE_FOR_IMAGES, FILE_TYPE_FOR_VIDEOS
 )
-from app.customizations.add_videos_and_thumbnails import REPOS_WITH_VIDEOS_OR_THUMBNAILS
+from app.customizations.add_videos_and_thumbnails import REPOS_WITH_THUMBNAILS, REPOS_WITH_VIDEOS 
 
 from app.functions.repos import add_repos_to_db, update_repo,get_repos_from_github_and_add_to_db
 from app.functions.repos import get_repos_from_github_and_add_to_db
@@ -13,17 +13,22 @@ from app import db
 def update_database():    
     get_repos_from_github_and_add_to_db()
 
-    # Add projects that don't have repos to the database
     add_repos_to_db(PROJECTS_WITHOUT_REPOS)
 
-    # Add 'priority' & 'highlighted' props to existing repos 
     for (repo, props) in REPOS_WITH_PRIORITY_OR_HIGHLIGHT.items():
         update_repo(repo, props)
 
-    # add 'thumbnail_filename' & 'video_filename' props to existing repos
-    for repo in REPOS_WITH_VIDEOS_OR_THUMBNAILS:
+    for repo in REPOS_WITH_THUMBNAILS:
         props = {
             'thumbnail_filename': f'{repo}{FILE_TYPE_FOR_IMAGES}',
+        }
+        update_repo(repo, props)
+
+    for repo in REPOS_WITH_VIDEOS:
+        props = {
             'video_filename': f'{repo}{FILE_TYPE_FOR_VIDEOS}'
         }
         update_repo(repo, props)
+
+
+    
